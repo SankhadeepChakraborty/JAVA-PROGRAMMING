87c4accdec8fe23df956f124b9771bde56bf18de
SankhadeepChakraborty <145654901+SankhadeepChakraborty@users.noreply.github.com>
2023-09-21 14:07:05 +0530

    Add files via upload

--- Addition.java
+++ Addition.java
@@ -1,12 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 
 class Addition{
     public static void main(String[]args){
-       Scanner sc = new Scanner(System.in);
+      
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the data a :");
         int a=sc.nextInt();
+       
+        System.out.println("Enter the data b :");
         int b=sc.nextInt();
         int sum=a+b;
-        System.out.println(sum);
+        System.out.println("Sum : "+sum);
     }
 
 }
