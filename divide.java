fc2f5481f6c4e6ac67fd41e6621f59ff1c018014
SankhadeepChakraborty <145654901+SankhadeepChakraborty@users.noreply.github.com>
2023-09-21 14:20:05 +0530

    Add files via upload

---
+++ divide.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class divide {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the data of a -");
+        int a= sc.nextInt();
+        System.out.print("Enter the data of b -");
+        int b=sc.nextInt();
+        int divide=a/b;
+        System.out.print("result is :"+divide);
+
+    }
+    
+}
